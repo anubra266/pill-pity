ab258f2d7cec11ad804051b5e453a5f706415411
Abraham <anubra266@gmail.com>
2022-01-30 15:27:36 +0100

    ✅ Nullify tests

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import PillPity from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<PillPity />, div);
+    ReactDOM.render(<div />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
